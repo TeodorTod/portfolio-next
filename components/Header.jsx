19d3f1a819f665d5a49b70df1a97d0bf91556ba4
TeodorTod <teodor.at.tod@gmail.com>
2024-08-31 17:05:07 +0300

    commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
                     <h1 className="text-4xl font-semibold">
-                        TTod123<span className="text-orange-400">.</span>
+                        TTod<span className="text-orange-400">.</span>
                     </h1>
                 </Link>
                 <div className="hidden xl:flex items-center gap-8">
