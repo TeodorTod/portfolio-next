d52b893cfb59ef72e791afe44c787ffd77a83094
TeodorTod <teodor.at.tod@gmail.com>
2024-09-13 23:34:30 +0300

    some fixes

--- app/page.jsx
+++ app/page.jsx
@@ -15,16 +15,26 @@ const Home = () => {
               Hello, I'm <br />
               <span className="text-accent">Teodor Todorov</span>{" "}
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">I'm working as a full-stack web developer and I am proficient in different technologies</p>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I'm working as a full-stack web developer and I am proficient in
+              different technologies
+            </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <a href="/assets/TeodorTodorovResume.pdf" download>
-                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
                   <span>Download CV</span>
                   <FiDownload className="text-xl" />
                 </Button>
               </a>
               <div className="mb-8 xl:mb-0">
-                <Socials containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
