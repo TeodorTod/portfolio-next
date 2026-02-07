92b29b4782978122ea4fdbeb5a3cf9e3d23c73cf
TeodorTod <teodor.at.tod@gmail.com>
2026-02-07 18:49:43 +0200

    me image and some head update

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
-import Head from 'next/head';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -14,15 +13,19 @@ const jetbrainsMono = JetBrains_Mono({
 export const metadata = {
   title: "Teodor Todorov | Web Developer Portfolio",
   description: "Explore the portfolio of Teodor Todorov, a full-stack web developer specializing in modern, responsive web applications. View services, skills, and contact information.",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
       <body className={jetbrainsMono.className}>
         <Header />
         <PageTransition>{children}</PageTransition>
